a3c64b5c7d63ba1eaae12b46d2a6a8d8f8f71cf1
samet.yigitkalan <sametykalan@gmail.com>
2026-01-09 17:25:53 +0300

    feat: being able to create executable

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,9 +1,9 @@
 import "electron";
 import "node:url";
 import "node:path";
-import { M, R, V } from "./main-UMpH4Cpg.js";
+import { M as m, R as p, V as t } from "./main-CUUYraI9.js";
 export {
-  M as MAIN_DIST,
-  R as RENDERER_DIST,
-  V as VITE_DEV_SERVER_URL
+  m as MAIN_DIST,
+  p as RENDERER_DIST,
+  t as VITE_DEV_SERVER_URL
 };
